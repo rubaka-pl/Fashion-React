6ca58c659ace6b83b7db445c32c76c2f391d6c2a
rubaka-pl <91855245+rubaka-pl@users.noreply.github.com>
2024-09-04 13:57:24 +0200

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,37 +1,57 @@
-import logoImg from "./../../img/icons/logo.svg"
-import './header.css'
-function Header() {
-    return <header className='header'>
-        <div className="container">
-            <div className="header__row">
-                <div className="header__logo">
-                    <img src={logoImg} alt='logo' />
-                    <span>Fashion</span>
-                </div>
-                <nav className="header__nav">
-                    <ul>
-                        <li>
-                            <a href="#!">CATALOGUE</a>
-                        </li>
-                        <li>
-                            <a href="#!">FASHION</a>
-                        </li>
-                        <li>
-                            <a href="#!">FAVOURITE</a>
-                        </li>
-                        <li>
-                            <a href="#!">LIFESTYLE</a>
-                        </li>
-                        <li>
-                            <a href="#!" className="header__navBtn">SIGN UP</a>
-                        </li>
-                    </ul>
+import React, { useState, useEffect } from 'react';
+import menuIcon from "./../../img/icons/menu.png";
+import closeIcon from "./../../img/icons/close.svg";
+import logoImg from "./../../img/icons/logo.svg";
+import './header.css';
 
-                </nav>
-                <div className="header__nav-btn">nav</div>
+const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    
+    const toggleMenu = () => {
+        setIsMenuOpen(prev => !prev);
+    };
+
+    // при клике на оверлей
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+    };
+
+
+    useEffect(() => {
+        document.body.style.overflow = isMenuOpen ? 'hidden' : 'auto';
+        return () => {
+            document.body.style.overflow = 'auto';
+        };
+    }, [isMenuOpen]);
+
+    return (
+        <header className='header'>
+            <div className="container">
+                <div className="header__row">
+                    <div className="header__logo">
+                        <img src={logoImg} alt='logo' />
+                        <span>Fashion</span>
+                    </div>
+                    <nav className={`header__nav ${isMenuOpen ? 'active' : ''}`}>
+                        <ul>
+                            <li><a href="#!">CATALOGUE</a></li>
+                            <li><a href="#!">FASHION</a></li>
+                            <li><a href="#!">FAVOURITE</a></li>
+                            <li><a href="#!">LIFESTYLE</a></li>
+                            <li>
+                                <a href="#!" className={`header__navBtn ${isMenuOpen ? 'active' : ''}`}>SIGN UP</a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div className={`header__nav-btn ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+                        <img src={isMenuOpen ? closeIcon : menuIcon} alt="menu" />
+                    </div>
+                </div>
             </div>
-        </div>
-    </header>
-}
+            {isMenuOpen && <div className="overlay" onClick={closeMenu}></div>}
+        </header>
+    );
+};
 
 export default Header;
